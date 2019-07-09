847c5980d7f020f07933ec9414964eabab399bf7
zhouli <563287128@qq.com>
2019-07-09 15:58:13 +0800

    添加joke内容，home小做尝试。

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
     <h1>home page</h1>
-    <p>something peanut want to say</p>
+    <p>welcome to peanuts' world!This is a place where I can say anything like NM$L & W$ND😶.</p>
+    <p>Coming soon...</p>
+    <div class="bouncing-loader">
+      <div />
+      <div />
+      <div />
+    </div>
   </div>
 </template>
 
@@ -16,5 +22,28 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+@keyframes bouncing-loader {
+  to {
+    opacity: 0.1;
+    transform: translate3d(0, -1rem, 0);
+  }
+}
+.bouncing-loader {
+  display: flex;
+  justify-content: center;
+}
+.bouncing-loader > div {
+  width: 1rem;
+  height: 1rem;
+  margin: 3rem 0.2rem;
+  background: #8385aa;
+  border-radius: 50%;
+  animation: bouncing-loader 0.6s infinite alternate;
+}
+.bouncing-loader > div:nth-child(2) {
+  animation-delay: 0.2s;
+}
+.bouncing-loader > div:nth-child(3) {
+  animation-delay: 0.4s;
+}
 </style>
--- src/components/Joke.vue
+++ src/components/Joke.vue
@@ -1,20 +1,81 @@
 <template>
-  <div>
-    <h1>Joke page</h1>
-    <p>happy time</p>
+  <div class="joke-container">
+    <h1 class="article-h1">
+      Peanut 🤣🤣🤣 Time !
+    </h1>
+    <blockquote
+      v-for="(i,index) in joke"
+      :key="index"
+    >
+      <p class="article-p">
+        {{ i.content }}
+        <span
+          :class="{fave:true,active:i.isLike}"
+          style="float:right;cursor:pointer"
+          @click="toggle(index)"
+        />
+      </p>
+    </blockquote>
   </div>
 </template>
 
 <script>
+import joke from '@/assets/joke'
 export default {
   data() {
     return {
-
+      joke
+    }
+  },
+  methods: {
+    toggle(index) {
+      this.joke[index].isLike = !this.joke[index].isLike
+      this.$notify({
+        title: '🍳🍳🍳',
+        message: '天天煎煎煎,NM$L',
+        duration: 0
+      })
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.joke-container{
+  text-align: left;
+  width: 690px;
+  .article-h1{
+    font-weight: 600;
+    font-synthesis: style;
+    font-size: 24px;
+    line-height: 1.22;
+    margin: 24px 0;
+  }
+  .article-p{
+    margin: 3.6em 0;
+    line-height: 1.6;
+  }
+  blockquote{
+    margin: 1.4em 0;
+    padding-left: 1em;
+    color: #646464;
+    border-left: 3px solid #d3d3d3;
+    .fave {
+      width: 25px;
+      height: 25px;
+      border-radius: 50%;
+      border: 1px solid #EA6F5A;
+      background: url(../assets/like_animation_steps.webp) no-repeat;
+      background-position: left;
+      background-size: auto 100%;
+    }
+    .fave.active {
+      background-color: #EA6F5A;
+      background-position: right;
+      /* 主要在这一步 */
+      transition: background .6s steps(19);
+    }
+  }
+}
 
 </style>
